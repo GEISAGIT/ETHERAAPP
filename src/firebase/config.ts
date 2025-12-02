8ad903fd995959059fc3bd9fef30923031fd78b1
Geisa Picksius Zardo <drageisazardo@gmail.com>
2025-12-02 18:49:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1445297951-c95ca",
+  "appId": "1:119110072700:web:3a34c37939f97831d10cf9",
+  "apiKey": "AIzaSyBpIHI7yKZpbtDSFXApzD0fxelRv-QsMew",
+  "authDomain": "studio-1445297951-c95ca.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "119110072700"
+};
