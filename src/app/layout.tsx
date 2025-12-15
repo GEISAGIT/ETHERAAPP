c47088e5b171203781376c5e3b36f6c7d0d0f316
Geisa Picksius Zardo <drageisazardo@gmail.com>
2025-12-15 13:13:41 +0000

    o icone do site continua aparecendo o do firebase, porem gostaria muito

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ import { ThemeProvider } from '@/components/layout/theme-provider';
 export const metadata: Metadata = {
   title: 'Ethera',
   description: 'Saúde e longevidade',
+  icons: {
+    icon: 'https://firebasestorage.googleapis.com/v0/b/studio-1445297951-c95ca.firebasestorage.app/o/assets%2Ffavicon-ethera.png?alt=media&token=8a5f3c5e-85da-49e3-8a8c-f2452b48348a',
+  },
 };
 
 export default function RootLayout({
@@ -17,7 +20,6 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="https://firebasestorage.googleapis.com/v0/b/studio-1445297951-c95ca.firebasestorage.app/o/assets%2Ffavicon-ethera.png?alt=media&token=8a5f3c5e-85da-49e3-8a8c-f2452b48348a" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
